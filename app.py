92d667f72283bd462e472ae16e97c60a2702e101
pestich <pestich@mail.ru>
2023-01-01 11:36:29 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from db import db
+from views import main_bp
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flask_app:flask_app_password@pg/flask_app'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    app.register_blueprint(main_bp)
+
+    return app
